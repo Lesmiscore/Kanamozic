6c6a54182b16dec37541c5ac73affd65fe7295b6
nao20010128@gmail.com <nao20010128@gmail.com>
2015-07-11 14:49:54 +0900

    いろいろ

--- src/com/nao20010128nao/Kanamozic/MainActivity.java
+++ src/com/nao20010128nao/Kanamozic/MainActivity.java
@@ -4,14 +4,32 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.widget.TextView.*;
 
 public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
+	EditText normal,encoded,caesar;
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		normal=(EditText)findViewById(R.id.normalText);
+		encoded=(EditText)findViewById(R.id.encodedText);
+		caesar=(EditText)findViewById(R.id.caesar);
+		findViewById(R.id.encodeButton).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View w){
+					encoded.setText(KanamozicCore.encode(normal.getText().toString(),getCaesar()),BufferType.EDITABLE);
+				}
+			});
+		findViewById(R.id.decodeButton).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View w){
+					normal.setText(KanamozicCore.decode(encoded.getText().toString(),getCaesar()),BufferType.EDITABLE);
+				}
+			});
     }
+	public byte getCaesar(){
+		return Byte.parseByte(caesar.getText().toString());
+	}
 }
